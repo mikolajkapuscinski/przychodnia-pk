ec1d46d42f43d6501a416e875a6969b5973ee48d
Jakub Bednarz <bednaszjakub@gmail.com>
2024-12-02 18:26:33 +0100

    feat: add colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,24 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      colors: {
+        'backgound': '#F1EFEE',
+        'orange': '#EF7158',
+        'light-orange': '#f39481',
+
+        'aquamarine': '#63A3B2',
+        'light-aquamarine': '#86B6C1',
+        'default-white': '#FBFBFB',
+        'default-gray': '#E5E0DD',
+
+        'default-black': '#443A35',
+        'light-brown': '#C1C1C1',
+        
+        'dark-yellow': '#FBD178',
+      },
+      borderRadius: {
+        DEFAULT: '.25rem',
+      },
     },
   },
   plugins: [],
