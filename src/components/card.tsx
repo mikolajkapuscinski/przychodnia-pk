c591406fd0376c5563ebb3a6bcf9cb5a1d4f83ae
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-12-11 16:23:25 +0100

    feat(ui): add possibility to extend card component's styles

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,14 +1,17 @@
 interface CardProps extends React.HTMLAttributes<HTMLBaseElement> {
   title: string;
+  className?: string;
 }
 
 export const Card: React.FC<CardProps> = (p: CardProps) => {
   return (
-    <div className="group relative w-52 cursor-pointer rounded-xl border-2 border-backgound p-4 shadow-lg">
+    <div
+      className={`group relative w-52 cursor-pointer rounded-xl border-2 border-backgound p-6 shadow-lg ${p.className}`}
+    >
       <h3 className="p-0 text-lg font-bold group-hover:text-aquamarine group-hover:underline">
         {p.title}
       </h3>
-      <div>{p.children}</div>
+      <div className="text-xs">{p.children}</div>
       <div className="absolute bottom-0 right-0 flex items-center justify-center rounded-br-xl rounded-tl-xl bg-aquamarine p-1 group-hover:bg-black">
         <img
           className="h-5 w-5 duration-300 group-hover:translate-x-1"
