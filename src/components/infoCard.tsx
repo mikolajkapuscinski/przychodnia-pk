f8c259d40f3b16a5eafd4764c18cfe1149356ee4
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-12-11 16:23:56 +0100

    refactor(ui): change width of infoCard

--- src/components/infoCard.tsx
+++ src/components/infoCard.tsx
@@ -4,7 +4,7 @@ export const InfoCard: React.FC<HTMLAttributes<HTMLBaseElement>> = (
   p: HTMLAttributes<HTMLBaseElement>,
 ) => {
   return (
-    <div className="group flex w-80 cursor-pointer items-center gap-3 rounded-xl bg-orange p-3 text-black shadow-md">
+    <div className="group flex w-[480px] cursor-pointer items-center gap-3 rounded-xl bg-orange p-3 capitalize text-black shadow-md">
       <div className="flex items-center justify-center p-1">
         <img className="h-5 w-5" src="/exc.png" alt=""></img>
       </div>
