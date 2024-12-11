3cfe6a94a1c785694c1b70f53567191ee1b73e43
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-12-11 16:25:37 +0100

    feat(ui): create Visit's card

---
+++ src/components/visitCard.tsx
@@ -0,0 +1,20 @@
+import { Card } from "./card";
+
+interface VisitCardProps extends React.HTMLAttributes<HTMLBaseElement> {
+  title: string;
+  className?: string;
+  isSoon: boolean;
+}
+
+export const VisitCard: React.FC<VisitCardProps> = (p: VisitCardProps) => {
+  const cardColor = p.isSoon ? "aquamarine" : "default-gray";
+
+  return (
+    <Card
+      className={`relative w-[480px] before:absolute before:left-0 before:top-0 before:h-full before:w-1 before:rounded-s-xl before:bg-${cardColor} before:content-[''] ${p.className}`}
+      title={p.title}
+    >
+      {p.children}
+    </Card>
+  );
+};
