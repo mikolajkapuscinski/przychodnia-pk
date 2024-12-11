52485cac711918ad1d7ca7c5d7160da2f774eb3d
Jakub Bednarz <bednaszjakub@gmail.com>
2024-12-11 22:16:48 +0100

    refactor: reorganize folder structure

--- src/pages/doctor/visit.tsx
+++ src/pages/doctor/visit.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NextPage } from "next";
-import { Visit } from "~/features/user/doctor/Visit";
+import { Visit } from "~/features/doctor/Visit";
 
 const VisitPage: NextPage = () => {
   return (
