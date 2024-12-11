f17f68ed5e117052bb2070bb372f9b9525256af2
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-12-11 11:45:36 +0100

    feat(ui): create infoCard component

---
+++ src/components/infoCard.tsx
@@ -0,0 +1,14 @@
+import { HTMLAttributes } from "react";
+
+export const InfoCard: React.FC<HTMLAttributes<HTMLBaseElement>> = (
+  p: HTMLAttributes<HTMLBaseElement>,
+) => {
+  return (
+    <div className="group flex w-80 cursor-pointer items-center gap-3 rounded-xl bg-orange p-3 text-black shadow-md">
+      <div className="flex items-center justify-center p-1">
+        <img className="h-5 w-5" src="/exc.png" alt=""></img>
+      </div>
+      <div>{p.children}</div>
+    </div>
+  );
+};
