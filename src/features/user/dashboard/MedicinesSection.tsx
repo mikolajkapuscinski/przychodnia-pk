61ec621b7e815e0bc3996cdfab660c52a1ebe25d
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-12-12 14:14:54 +0100

    feat(ui): add elements to section

--- src/features/user/dashboard/MedicinesSection.tsx
+++ src/features/user/dashboard/MedicinesSection.tsx
@@ -1,4 +1,4 @@
-import { Card } from "~/components/card";
+import { DrugCard } from "~/components/cards/drugCard";
 import { SectionTitle } from "~/components/dashboard/SectionTitle";
 
 export const MedicinesSection: React.FC = () => {
@@ -6,9 +6,20 @@ export const MedicinesSection: React.FC = () => {
     <div className="py-4">
       <SectionTitle>Medications</SectionTitle>
       <div className="grid place-items-center items-stretch gap-x-2 gap-y-3 lg:grid-cols-1 2xl:grid-cols-2">
-        <Card title="Paracetamol"></Card>
-        <Card title="Fentanyl"></Card>
-        <Card title="Rutinoscorbin"></Card>
+        <DrugCard title="Paracetamol">
+          <img
+            className="w-full rounded-lg"
+            src="/medicine-1.jpeg"
+            alt="drug"
+          />
+        </DrugCard>
+        <DrugCard title="Fentanyl">
+          <img
+            className="rounded-lg"
+            src="/medicine-2.jpg"
+            alt="drug"
+          />
+        </DrugCard>
       </div>
     </div>
   );
--- src/features/user/dashboard/visitSection.tsx
+++ src/features/user/dashboard/visitSection.tsx
@@ -15,8 +15,6 @@ export const VisitSection: React.FC = () => {
       ></DatePicker>
       <div className="grid grid-cols-1 place-items-center items-stretch gap-x-2 gap-y-3">
         <VisitCard isSoon={true} title="MRI: right thigh, knee and shin"></VisitCard>
-        <VisitCard isSoon={false} title="Consultation: surgery preparation"></VisitCard>
-        <VisitCard isSoon={false} title="Consultation: physical rehabilitation before & after surgery"></VisitCard>
       </div>
     </div>
   );
