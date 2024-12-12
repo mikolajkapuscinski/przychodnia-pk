d3c572c03ec0ad5c57f97f30ff1466e48f4fe334
Jakub Bednarz <bednaszjakub@gmail.com>
2024-12-12 10:31:12 +0100

    feat(ui): add doctor's visits section

--- src/features/doctor/dashboard/PatientsSection.tsx
+++ src/features/doctor/dashboard/PatientsSection.tsx
@@ -8,16 +8,30 @@ export const PatientSection: React.FC<PatientSectionProps> = (
 ) => {
   return (
     <div>
-      <SectionTitle title={"My Patients"} results={1}></SectionTitle> <></>
-      <PatientBlock
-        name={"Boa Dusiciel"}
-        onViewHistory={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-        pesel={"02322203534"}
-        age={23}
-        gender={"M"}
-      />
+      <SectionTitle results={1}>My Patients</SectionTitle>
+      <div className="flex flex-col gap-y-2">
+        <PatientBlock
+          name={"Boa Dusiciel"}
+          onViewHistory={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          pesel={"02322203534"}
+        />
+        <PatientBlock
+          name={"Bożydar Kleks"}
+          onViewHistory={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          pesel={"95884654231"}
+        />
+        <PatientBlock
+          name={"Julian Tuwim"}
+          onViewHistory={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+          pesel={"12356489877"}
+        />
+      </div>
     </div>
   );
 };
--- src/features/doctor/dashboard/VisitsSection.tsx
+++ src/features/doctor/dashboard/VisitsSection.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { SectionTitle } from "~/components/dashboard/SectionTitle";
+import { VisitCard } from "~/components/visitCard";
 import { DatePicker } from "~/features/user/create-visit/DatePicker";
 
 interface VisitsSectionProps {}
@@ -6,9 +8,30 @@ interface VisitsSectionProps {}
 export const VisitsSection: React.FC<VisitsSectionProps> = (
   p: VisitsSectionProps,
 ) => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   return (
     <>
-      <SectionTitle title={"My Visits"} results={1}></SectionTitle>
+      <div className="">
+        <SectionTitle results={1}>My Visits</SectionTitle>
+        <DatePicker
+          selectedDate={selectedDate}
+          onDateChange={setSelectedDate}
+        ></DatePicker>
+        <div className="grid grid-cols-1 place-items-center items-stretch gap-x-2 gap-y-3">
+          <VisitCard
+            isSoon={true}
+            title="MRI: right thigh, knee and shin"
+          ></VisitCard>
+          <VisitCard
+            isSoon={false}
+            title="Consultation: surgery preparation"
+          ></VisitCard>
+          <VisitCard
+            isSoon={false}
+            title="Consultation: physical rehabilitation before & after surgery"
+          ></VisitCard>
+        </div>
+      </div>
     </>
   );
 };
