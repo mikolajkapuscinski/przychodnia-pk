58a0a6516c69f713b60d629d0a30ca0bd446c47e
Michał Jabczyk <13063089+jabczyk@users.noreply.github.com>
2024-12-12 14:54:32 +0100

    refactor(user): turn user modules into classes

--- src/server/api/user/user.access.ts
+++ src/server/api/user/user.access.ts
@@ -8,117 +8,124 @@ type CreateUser = Omit<Prisma.UserCreateInput, "passwordHash"> & {
   password: string;
 };
 
-const PUBLIC_USER_FIELDS = {
-  id: true,
-  firstName: true,
-  lastName: true,
-  email: true,
-  phoneNumber: true,
-} as const satisfies Prisma.UserSelect;
+export class UserAccess {
+  private static PUBLIC_USER_FIELDS = {
+    id: true,
+    firstName: true,
+    lastName: true,
+    email: true,
+    phoneNumber: true,
+  } as const satisfies Prisma.UserSelect;
 
-export const createUser = async (createUser: CreateUser) => {
-  const { password, ...userData } = createUser;
-  const data: Prisma.UserCreateInput = {
-    ...userData,
-    passwordHash: await hash(password),
-  };
+  public async createUser(createUser: CreateUser): Promise<string> {
+    const { password, ...userData } = createUser;
+    const data: Prisma.UserCreateInput = {
+      ...userData,
+      passwordHash: await hash(password),
+    };
 
-  if (createUser.role == UserRole.DOCTOR) {
-    data.doctor = {
-      create: {},
-    };
-  } else if (createUser.role == UserRole.PATIENT) {
-    data.patient = {
-      create: {},
-    };
+    if (createUser.role == UserRole.DOCTOR) {
+      data.doctor = {
+        create: {},
+      };
+    } else if (createUser.role == UserRole.PATIENT) {
+      data.patient = {
+        create: {},
+      };
+    }
+
+    const user = await db.user.create({
+      data,
+    });
+
+    return user.id;
   }
 
-  const user = await db.user.create({
-    data,
-  });
+  public async findUserById(id: string) {
+    const user = await db.user.findUnique({
+      where: { id },
+    });
+    assert(user, "User not found");
 
-  return user.id;
-};
+    return user;
+  }
 
-export const findUserById = async (id: string) => {
-  const user = await db.user.findUnique({
-    where: { id },
-  });
-  assert(user, "User not found");
+  public async findUserByEmail(email: string) {
+    const user = await db.user.findUnique({
+      where: { email },
+    });
+    assert(user, "User not found");
 
-  return user;
-};
+    return user;
+  }
 
-export const findUserByEmail = async (email: string) => {
-  const user = await db.user.findUnique({
-    where: { email },
-  });
-  assert(user, "User not found");
+  public async findPublicUserByPesel(pesel: string) {
+    const user = await db.user.findUnique({
+      where: { pesel },
+      select: UserAccess.PUBLIC_USER_FIELDS,
+    });
+    assert(user, "User not found");
 
-  return user;
-};
+    return user;
+  }
 
-export const findPublicUserByPesel = (pesel: string) => {
-  const user = db.user.findUnique({
-    where: { pesel },
-    select: PUBLIC_USER_FIELDS,
-  });
-  assert(user, "User not found");
-
-  return user;
-};
-
-export const findDoctors = async () => {
-  const users = await db.user.findMany({
-    where: { role: UserRole.DOCTOR },
-    select: {
-      ...PUBLIC_USER_FIELDS,
-      doctor: {
-        select: {
-          specialization: true,
+  public async findDoctors() {
+    const users = await db.user.findMany({
+      where: { role: UserRole.DOCTOR },
+      select: {
+        ...UserAccess.PUBLIC_USER_FIELDS,
+        doctor: {
+          select: {
+            specialization: true,
+          },
         },
       },
-    },
-  });
+    });
+    assert(users);
 
-  const opinions = await getOpinionSummary();
+    const opinions = await getOpinionSummary();
+    assert(opinions);
 
-  return users.map((user) => {
-    return {
-      id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      phoneNumber: user.phoneNumber,
-      specialization: user.doctor?.specialization ?? [],
-      opinions: opinions[user.id],
-    };
-  });
-};
+    return users.map((user) => {
+      return {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        specialization: user.doctor?.specialization ?? [],
+        opinions: opinions[user.id],
+      };
+    });
+  }
 
-export const updateUser = async (
-  userId: string,
-  updatedUser: Prisma.UserUpdateInput,
-) => {
-  const user = await db.user.update({
-    where: { id: userId },
-    data: updatedUser,
-    select: { id: true },
-  });
-  assert(user, "User not found");
+  public async updateUser(
+    userId: string,
+    updatedUser: Prisma.UserUpdateInput,
+  ): Promise<string> {
+    const user = await db.user.update({
+      where: { id: userId },
+      data: updatedUser,
+      select: { id: true },
+    });
+    assert(user, "User not found");
 
-  return user.id;
-};
+    return user.id;
+  }
 
-export const updatePassword = async (userId: string, password: string) => {
-  const user = await db.user.update({
-    where: { id: userId },
-    data: {
-      passwordHash: await hash(password),
-    },
-    select: { id: true },
-  });
-  assert(user, "User not found");
+  public async updatePassword(
+    userId: string,
+    password: string,
+  ): Promise<string> {
+    const user = await db.user.update({
+      where: { id: userId },
+      data: {
+        passwordHash: await hash(password),
+      },
+      select: { id: true },
+    });
+    assert(user, "User not found");
 
-  return user.id;
-};
+    return user.id;
+  }
+}
