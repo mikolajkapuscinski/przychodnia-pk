de094fbc8db578d748e7bfa811c6f416a9bf75ce
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-12-12 14:55:36 +0100

    refactor(ui): change infoCard's content color

--- src/components/cards/infoCard.tsx
+++ src/components/cards/infoCard.tsx
@@ -8,10 +8,10 @@ export const InfoCard: React.FC<HTMLAttributes<HTMLBaseElement>> = (
   return (
     <div
       onClick={() => setclicked(true)}
-      className={`group w-full cursor-pointer items-center gap-3 rounded-xl bg-orange p-3 capitalize text-black shadow-md ${clicked ? "hidden" : "flex"}`}
+      className={`group w-full cursor-pointer items-center gap-3 rounded-xl bg-orange p-3 capitalize text-default-white shadow-md ${clicked ? "hidden" : "flex"}`}
     >
       <div className="flex items-center justify-center p-1">
-        <img className="h-5 w-5" src="/exc.png" alt=""></img>
+        <img className="invert h-5 w-5" src="/exc.png" alt=""></img>
       </div>
       <div>{p.children}</div>
     </div>
