299f7b5e9e145591d34a1f8a271214f4dc102ea8
Jakub Bednarz <bednaszjakub@gmail.com>
2024-12-14 11:57:13 +0100

    feat(ui): add Custom Dialog Panel

---
+++ src/components/CustomDialog.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Transition, Dialog, DialogPanel } from "@headlessui/react";
+import { Button } from "./forms/Button";
+
+interface CustomDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  children: React.ReactNode;
+}
+
+export const CustomDialog: React.FC<CustomDialogProps> = ({
+  isOpen,
+  onClose,
+  children,
+}) => {
+  return (
+    <Transition show={isOpen} as={React.Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={onClose}>
+        {/* Overlay */}
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50"
+          aria-hidden="true"
+        />
+
+        {/* Dialog Panel */}
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <DialogPanel className="min-w-4xl relative w-full max-w-4xl rounded-lg bg-white p-6 shadow-xl">
+            {/* Close Button */}
+            <Button
+              onClick={onClose}
+              className="absolute right-4 top-4"
+              variant={"secondary"}
+              size={"xs"}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </Button>
+
+            {/* Dialog Content */}
+            {children}
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
