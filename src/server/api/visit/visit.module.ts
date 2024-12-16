84f135cf0128e429aea2ab3ff453add33b98ec4a
Michał Jabczyk <13063089+jabczyk@users.noreply.github.com>
2024-12-16 14:54:25 +0100

    fix(visit): add drug access to module

--- src/server/api/visit/visit.module.ts
+++ src/server/api/visit/visit.module.ts
@@ -1,6 +1,7 @@
 import { ReflectiveInjector } from "injection-js";
 import { VisitAccess } from "./visit.access";
 import { ManageVisitEngine } from "./manage-visit.engine";
+import { DrugAccess } from "../drug/drug.access";
 
-export const visitElements = [VisitAccess, ManageVisitEngine];
+export const visitElements = [VisitAccess, ManageVisitEngine, DrugAccess];
 export const visitInjector = ReflectiveInjector.resolveAndCreate(visitElements);
