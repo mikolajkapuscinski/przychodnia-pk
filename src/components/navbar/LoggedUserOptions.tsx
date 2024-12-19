a602543e503a28c4f8c0001ad311312fda2a0ee0
mikolajkapuscinski <mikolajkapuscinski40@gmail.com>
2024-12-19 21:06:26 +0100

    refactor(navabar): edit logged user options

--- src/components/navbar/LoggedUserOptions.tsx
+++ src/components/navbar/LoggedUserOptions.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Popover, PopoverPanel } from "@headlessui/react";
 import { Button } from "../forms/Button";
 import { Line } from "../forms/Line";
 import { useSession, signOut } from "next-auth/react";
+import { CustomDialog } from "../CustomDialog";
+import { EditProfileForm } from "../forms/EditProfileForm";
+import { ChangePasswordForm } from "../forms/ChangePasswordForm";
 
 interface LoggedUserOptionsProps {
   isOpen: boolean;
@@ -14,61 +17,72 @@ export const LoggedUserOptions: React.FC<LoggedUserOptionsProps> = ({
   onClose,
 }) => {
   const { data: sessionData } = useSession();
+
+  const [isChangePassOpen, setChangePassOpen] = useState(false);
+  const [isEditProfileOpen, setEditProfileOpen] = useState(false);
+
   return (
-    <Popover as="div" className="relative z-50">
-      {isOpen && (
-        <PopoverPanel
-          static
-          className="absolute right-0 mt-10 w-64 rounded-lg bg-white shadow-xl"
-        >
-          <div className="relative">
-            <div className="absolute -top-2 right-10 h-4 w-4 rotate-45 transform bg-white"></div>
+    <>
+      <Popover as="div" className="relative z-50">
+        {isOpen && (
+          <PopoverPanel
+            static
+            className="absolute right-0 mt-10 w-64 rounded-lg bg-white shadow-xl"
+          >
+            <div className="relative">
+              <div className="absolute -top-2 right-10 h-4 w-4 rotate-45 transform bg-white"></div>
 
-            <div className="p-4">
-              <div className="text-center">
-                <h3 className="text-lg font-semibold text-gray-800">
-                  {sessionData?.user?.name ?? "Mariusz Rynkiewicz"}
-                </h3>
-                <p className="text-sm text-gray-500">
-                  {sessionData?.user?.email ?? "mariusz@rynkiewicz.com"}
-                </p>
+              <div className="p-4">
+                <div className="text-center">
+                  <h3 className="text-lg font-semibold text-gray-800">
+                    {sessionData?.user?.name ?? "Mariusz Rynkiewicz"}
+                  </h3>
+                  <p className="text-sm text-gray-500">
+                    {sessionData?.user?.email ?? "mariusz@rynkiewicz.com"}
+                  </p>
+                </div>
+
+                <Line />
+
+                <Button
+                  variant="secondary"
+                  size="base"
+                  className="flex w-full items-center justify-start"
+                  onClick={() => setEditProfileOpen(true)}
+                >
+                  Edit Profile
+                </Button>
+                <Button
+                  variant="secondary"
+                  size="base"
+                  className="flex w-full items-center justify-start"
+                  onClick={() => setChangePassOpen(true)}
+                >
+                  Change Password
+                </Button>
+
+                <Button
+                  variant="secondary"
+                  size="base"
+                  className="flex w-full items-center justify-start text-red-500 hover:bg-red-500 hover:text-white"
+                  onClick={() => {
+                    onClose();
+                    signOut();
+                  }}
+                >
+                  Log Out
+                </Button>
               </div>
-
-              <Line />
-
-              <Button
-                variant="secondary"
-                size="base"
-                className="flex w-full items-center justify-start"
-                onClick={onClose}
-              >
-                My Profile
-              </Button>
-
-              <Button
-                variant="secondary"
-                size="base"
-                className="flex w-full items-center justify-start"
-                onClick={onClose}
-              >
-                Edit Profile
-              </Button>
-
-              <Button
-                variant="secondary"
-                size="base"
-                className="flex w-full items-center justify-start text-red-500 hover:bg-red-500 hover:text-white"
-                onClick={() => {
-                  onClose();
-                  signOut();
-                }}
-              >
-                Log Out
-              </Button>
             </div>
-          </div>
-        </PopoverPanel>
-      )}
-    </Popover>
+          </PopoverPanel>
+        )}
+      </Popover>
+      <CustomDialog isOpen={isEditProfileOpen} onClose={() => setEditProfileOpen(false)}>
+        <EditProfileForm></EditProfileForm>
+      </CustomDialog>
+      <CustomDialog isOpen={isChangePassOpen} onClose={() => setChangePassOpen(false)}>
+        <ChangePasswordForm></ChangePasswordForm>
+      </CustomDialog>
+    </>
   );
 };
